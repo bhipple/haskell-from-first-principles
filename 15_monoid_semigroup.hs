4d6e8c95a5b7b1e2dfedbcef67615ef2dd254c72
Benjamin Hipple <brh2113@columbia.edu>
2016-04-29 20:06:30 -0400

    working on ch15

--- 15_monoid_semigroup.hs
+++ 15_monoid_semigroup.hs
@@ -33,3 +33,25 @@ instance Monoid a => Monoid (Optional a) where
     mappend (Only x) Nada = Only x
     mappend Nada (Only x) = Only x
     mappend (Only x) (Only y) = Only (mappend x y)
+
+
+-- Mad Lib Example
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbin' e adv noun adj =
+    e <> "! he said " <> adv <> " as he jumped into his car "
+    <> noun <> " and drove off with his " <> adj <> " wife."
+
+madlibbinBetter' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter' e adv noun adj =
+    mconcat [e, "! he said ", adv, " as he jumped into his car ", noun,
+             " and drove off with his ", adj, " wife."]
